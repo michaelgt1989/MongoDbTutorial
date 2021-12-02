3fb4a46d70254eb5c681115301419f7aab66db9a
Michael George-Taylor <michael.gt1989@gmail.com>
2021-12-02 15:19:29 -0700

    final commit on tutorial

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); 
+const Joi = require('joi');
  
 const productSchema = new mongoose.Schema({ 
   name: { type: String, required: true, minlength: 2, maxlength: 255 }, 
@@ -9,5 +10,19 @@ const productSchema = new mongoose.Schema({
 });
 
 const Product = mongoose.model('Product', productSchema);
+
+function validateProduct(product) { 
+    const schema = Joi.object({ 
+      name: Joi.string().min(2).max(50).required(), 
+      description: Joi.string().required(), 
+      category: Joi.string().min(5).max(50).required(), 
+      price: Joi.number().required(), 
+    }); 
+    return schema.validate(product); 
+} 
+   
+  exports.Product = Product; 
+  exports.validate = validateProduct; 
+  exports.productSchema = productSchema;
 
 module.exports = Product;
--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,88 @@
 const Product = require('../models/product'); 
 const express = require('express'); 
 const router = express.Router(); 
- 
-router.post('/', async (req, res) => { 
+const { Product, validate } = require('../models/product');
+
+router.get('/', async (req, res) => { 
+    try { 
+      const products = await Product.find(); 
+      return res.send(products); 
+    } catch (ex) { 
+      return res.status(500).send(`Internal Server Error: ${ex}`); 
+    } 
+  });
+
+  router.get('/:id', async (req, res) => { 
     try { 
          
+      const product = await Product.findById(req.params.id); 
+   
+      if (!product)  
+          return res.status(400).send(`The product with id "${req.params.id}" does not exist.`); 
+   
+      return res.send(product); 
+   
+    } catch (ex) { 
+      return res.status(500).send(`Internal Server Error: ${ex}`); 
+    } 
+  });
+ 
+router.post('/', async (req, res) => { 
+    try {
+        const { error } = validate(req.body); 
+        if (error)  
+            return res.status(400).send(error); 
+       
       const product = new Product({ 
-        name: 'Stanley Classic Vacuum Bottle', 
-        description: `Our Stanley Classic Vacuum Bottle is made with superior  
-  insulation that keeps liquids (soup, coffee, tea) hot or cold drinks cool for up 
-  to 24 hours.`, 
-        category: 'Travel', 
-        price: 19.82, 
+        name: req.body.name, 
+        description: req.body.description, 
+        category: req.body.category, 
+        price: req.body.price, 
       }); 
    
       await product.save(); 
+   
+      return res.send(product); 
+    } catch (ex) { 
+      return res.status(500).send(`Internal Server Error: ${ex}`); 
+    } 
+  });
+
+router.put('/:id', async (req, res) => { 
+    try { 
+      const { error } = validate(req.body); 
+      if (error) return res.status(400).send(error); 
+   
+      const product = await Product.findByIdAndUpdate( 
+        req.params.id, 
+        { 
+          name: req.body.name, 
+          description: req.body.description, 
+          category: req.body.category, 
+          price: req.body.price, 
+        }, 
+        { new: true } 
+      ); 
+   
+      if (!product)  
+          return res.status(400).send(`The product with id "${req.params.id}" does not exist.`); 
+   
+      await product.save(); 
+   
+      return res.send(product); 
+    } catch (ex) { 
+      return res.status(500).send(`Internal Server Error: ${ex}`); 
+    } 
+  });
+
+router.delete('/:id', async (req, res) => { 
+    try { 
+         
+      const product = await Product.findByIdAndRemove(req.params.id); 
+   
+      if (!product)  
+          return res.status(400).send(`The product with id "${req.params.id}" d
+  oes not exist.`); 
    
       return res.send(product); 
    
